58e18dbe56ab938c507620a5b03f9be3abee5776
TyCoding <2557988481@qq.com>
2018-05-04 18:58:30 +0800

    SSM框架整合（分页查询）案例

--- src/main/java/com/TyCoding/controller/UserController.java
+++ src/main/java/com/TyCoding/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 用户的控制层
@@ -25,23 +26,18 @@ public class UserController {
      * 用户登录
      */
     @RequestMapping(value = "/login")
-    public String login(User user, Model model) {
+    public String login(@RequestParam String username,@RequestParam String password, Model model) {
+        User user = userService.login(username);
         if (user != null) {
-            User userResult = userService.login(user.getUsername());
-            if(userResult != null){
-                if (userResult.getPassword().equals(user.getPassword())) {
-                    //登录成功
-                    return "page/page";
-                } else {
-                    model.addAttribute("message", "登录失败");
-                    return "page/loginInfo";
-                }
-            }else{
-                model.addAttribute("message","登录失败");
+            if (user.getPassword().equals(password)) {
+                //登录成功
+                return "page/page";
+            } else {
+                model.addAttribute("message", "登录失败");
                 return "page/loginInfo";
             }
         } else {
-            model.addAttribute("message", "你输入的信息为空");
+            model.addAttribute("message", "你输入的用户名或密码有误");
             return "page/loginInfo";
         }
     }
